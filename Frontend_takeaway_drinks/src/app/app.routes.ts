1dd5d28bb0da2b6f5b45f0b57b93823ab0f7faf0
NGUYEN DINH THIEM <bestboykai97@gmail.com>
2026-08-30 03:17:23 +0700

    delete code no use

--- Frontend_takeaway_drinks/src/app/app.routes.ts
+++ Frontend_takeaway_drinks/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Detail } from './detail/detail';
 import { Create } from './create/create';
-import { LifecycleChild } from './lifecycle-child/lifecycle-child';
-import { LifecycleParent } from './lifecycle-parent/lifecycle-parent';
 
 export const routes: Routes = [
     {
@@ -20,10 +18,5 @@ export const routes: Routes = [
         path: 'create',
         loadComponent: () =>
             import('./create/create').then(m => m.Create)
-    },
-    {
-        path: 'child',
-        loadComponent: () =>
-            import('./lifecycle-child/lifecycle-child').then(m => m.LifecycleChild)
     },
 ];
